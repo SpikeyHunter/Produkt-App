119bf0187f63463051043183a70ac66d4b6d054c
SpikeyHunter <charleslbr@icloud.com>
2025-09-22 18:26:15 -0400

    DevBuild_0.1.4
    Fixed SVG icon

--- src/routes/wifi/+page.svelte
+++ src/routes/wifi/+page.svelte
@@ -320,7 +320,7 @@
               <span>{t.connecting}</span>
             {:else}
               <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.111 16.404a5.5 5.5 0 717.778 0M12 20h.01m-7.08-7.071c3.904-3.905 10.236-3.905 14.141 0M1.394 9.393c5.857-5.857 15.355-5.857 21.213 0"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.111 16.404a5.5 5.5 0 017.778 0M12 20h.01m-7.08-7.071c3.904-3.905 10.236-3.905 14.141 0M1.394 9.393c5.857-5.857 15.355-5.857 21.213 0"></path>
               </svg>
               <span>{t.connect}</span>
             {/if}
