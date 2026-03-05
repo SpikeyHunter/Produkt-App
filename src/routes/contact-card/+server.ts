5d183edc5f2696f844eafb6ff49cc4de03a4894c
Charles <43152508+SpikeyHunter@users.noreply.github.com>
2026-03-05 01:31:15 -0500

    DevBuild_2.0.8D
    Updated Contact Card

--- src/routes/contact-card/+server.ts
+++ src/routes/contact-card/+server.ts
@@ -3,7 +3,7 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
     // Your long Supabase URL
-    const supabaseVcfUrl = 'https://vngekjtqbdnfeombtjnx.supabase.co/storage/v1/object/public/public-assets/calendar/contact_card/Produkt%20Calendar.vcf';
+    const supabaseVcfUrl = 'https://vngekjtqbdnfeombtjnx.supabase.co/storage/v1/object/public/public-assets/calendar/contact_card/Produkt%20App.vcf';
     
     // 302 Redirect sends the user straight to the file to download it
     throw redirect(302, supabaseVcfUrl);
